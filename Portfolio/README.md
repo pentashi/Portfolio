eacff16869e5b02107339c6e9ab1fc80498773ff
pentashi <achapipentashi@gmail.com>
2025-04-20 16:03:42 +0100

    Re-added Portfolio as a regular folder

---
+++ Portfolio/README.md
@@ -0,0 +1,40 @@
+<h2 align="center"> Portfolio Website - v2.0<br/> <a href="https://your-portfolio-link.com" target="_blank">https://your-portfolio-link.com</a> </h2> <div align="center"> <img alt="Demo" src="./Images/readme-img1.png" /> </div> <br/> <center>
+   
+ 
+
+</center> <h3 align="center"> 🔹 <a href="https://github.com/pentashi/Portfolio/issues">Report Bug</a> &nbsp; &nbsp; 🔹 <a href="https://github.com/pentashi/Portfolio/issues">Request Feature</a> </h3>
+TL;DR
+You can fork this repo to modify and make changes of your own. Please give proper credit by linking back to Mbongwe Brandon Egbe. Thanks!
+
+Built With
+My personal portfolio <a href="https://your-portfolio-link.com" target="_blank">https://your-portfolio-link.com</a> features some of my GitHub projects as well as my resume and technical skills.
+
+This project was built using these technologies:
+
+React.js
+Node.js
+Express.js
+CSS3
+VsCode
+Vercel
+Features
+📖 Multi-Page Layout
+🎨 Styled with React-Bootstrap and CSS with easy-to-customize colors
+📱 Fully Responsive
+
+Getting Started
+Clone this repository. You will need node.js and git installed globally on your machine.
+
+🛠 Installation and Setup Instructions
+Installation: npm install
+In the project directory, you can run: npm start
+This runs the app in development mode.
+Open http://localhost:3000 to view it in the browser. The page will reload if you make edits.
+
+Usage Instructions
+Open the project folder and navigate to /src/components/.
+You will find all the components used, and you can edit your information accordingly.
+
+Show your support
+Give a ⭐ if you like this website!
+
---
+++ Portfolio/src/components/Projects/Projects.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+// import leaf from "../../Assets/Projects/leaf.png";
+import digitalclock from "../../Assets/Projects/digitalclock.png";
+import stopwatch from "../../Assets/Projects/stopwatch.png";
+import todo from "../../Assets/Projects/todo.png";
+import Cammarket from "../../Assets/Projects/Cammarket.png";
+import Moovestudent from "../../Assets/Projects/Moovestudent.png";
+import { BsStopwatch } from "react-icons/bs";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+  <ProjectCard
+    imgPath={Ecommerce} // replace with your eCommerce project's image path
+    isBlog={false}
+    title="Ecommerce-Platform" // replace with your eCommerce project's title
+    description="A user-friendly eCommerce platform designed to provide an engaging shopping experience and modernized transaction processes." 
+    ghLink="https://github.com/pentashi/ecommerce-" // replace with your GitHub link
+    demoLink="https://ecommerce-omega-swart-83.vercel.app/" // replace with your demo link
+  />
+</Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Moovestudent}
+              isBlog={false}
+              title="Moove-Student"
+              description="Landing page for a comprehensive solution for modernizing educational processes and enhancing student engagement."
+              ghLink="https://github.com/pentashi/Moove-Student"
+              demoLink="https://moove-student.vercel.app/"
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Cammarket}
+              isBlog={false}
+              title="Cammarket"
+              description="Online market platform Landing page "
+              ghLink="https://github.com/pentashi/Cammarket"
+              demoLink="https://cammarket.vercel.app/"
+            />
+          </Col> */}
+{/* 
+          <Col md={4} className="Stop watch">
+            <ProjectCard
+              imgPath={stopwatch}
+              isBlog={false}
+              title="Stopwatch"
+              description="Online Digital stopwatch"
+              ghLink="https://github.com/pentashi/achapi-stop-watch"
+              demoLink="https://achapi-stop-watch2.vercel.app/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={digitalclock}
+              isBlog={false}
+              title=" Modern Digital Clock"
+              description="A modern digital clock built with react that permits you to see the time of various timezones"
+              ghLink="https://github.com/pentashi/achapi-digital-clock"
+              demoLink="https://achapi-digital-clock.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={todo}
+              isBlog={false}
+              title="Todo"
+              description="An App that helps you manage your task and Schedule activities"
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+               demoLink="https://task-management-ui-blond.vercel.app/"
+            />
+          </Col> */}
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              // imgPath={SER}
+              isBlog={false}
+              title="Speech Emotion Detection"
+              description="This is a speech emotion detection model i trained with tensorflow and saved in keras and integrated it into an android app to inteprete emotions in real time0"
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
